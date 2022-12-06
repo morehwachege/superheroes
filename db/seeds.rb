50497d72e7d82ccc33104b834cbdaad723648dbb
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-12-06 15:27:41 +0300

    add: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "✍🏻Start seeding...📝"
+puts "======================================================="
+(1..10).map do |i|
+    hero = Hero.create(
+        name: Faker::Movies::HarryPotter.character, 
+        super_name: Faker::Fantasy::Tolkien.character
+    )
+    hero_power = HeroPower.create(
+        strength: ['Strong', 'Weak', 'Average'].sample,
+        hero_id: rand(1..10),
+        power_id: rand(1..10)
+    )
+
+    power = Power.create(
+        name: Faker::Movies::HarryPotter.spell,
+        description: Faker::Movies::HarryPotter.quote
+    )
+
+    
+end
+
+puts "✅ Done seeding 🌱"
